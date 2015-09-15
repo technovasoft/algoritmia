a8c99fe8a5f881144d1543f54dbc663b5de3e9da
Ruiz Valdivia Jose Ernesto <senjuana@users.noreply.github.com>
2015-09-14 20:35:27 -0500

    Update Bloque2.py

--- Bloque2.py
+++ Bloque2.py
@@ -1,1 +1,20 @@
 
+#numero=input("Dame el primer numero:")
+#numero2=input("Dame el segundo numero:")
+#numero3=input("Dame el tercer numero:")
+#numero4=input("Dame el cuarto numero:")
+#numero5=input("Dme el quinto numero:")
+
+#numero1a=int(numero)
+#numero2a=int(numero2)
+#numoro3a=int(numero3)  #python no es tan cool por esta mierda
+#numero4a=int(numero4)
+#numero5a=int(numero5)
+
+#promedio=((numero1a+numero2a+numoro3a+numero4a+numero5a)/5)
+
+
+
+#print("El promedio es:",promedio)
+
+#Todo esto es un ejemplo de media aritmetica tienen que hacerlo metodo para 'n' numeros de entrada.
