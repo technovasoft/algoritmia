6608d8af68c0e24a90fbd0abeac86f0610c0f6c1
Carlos Hernández <carlos.hernandez.sc@itszapopan.edu.mx>
2015-10-11 21:45:52 -0500

    Update Bloque1.py

--- Bloque1.py
+++ Bloque1.py
@@ -1,4 +1,31 @@
-#Carlos cabron mete tus codigos aqui
-#solo le das a editar y dropeas tu codigo morro
-# y lo guardas wau puta magia.
+#Compañeros una disculpa por la tardanza pero por motivos de que no estoy en mi casa no he podido subir nada, por el momento subo esta parte que tenia guardada en mi celular, en el transcurso de la madrugada subire lo demas, Erni no te enojes wey, tengo algunos problemas y con trabajos puedo subir esto.
+#Cambio y fuera;
 
+
+print ("BLOQUE 1")
+print ("CONCEPTOS GENERALES\n")
+
+###########ALGORITMO
+for rep in range (0,5):
+    
+    n1 = input("Ingresa la cantidad de campeonatos ganados:\n>>> ")
+    n2 = input("Ingresa la cantidad total de campeonatos ganados:\n>>> ")
+    n3 = float(n1)
+    n4 = float(n2)
+    operacion = (n3 * 100 / n4)
+    operacion2 = round(operacion,2 ) #Round sirve para redondear una cantidad que se imprime y ",2" sirve para solo imprimir dos decimales.
+    print (operacion2)
+    
+############TABLA CON LOS DATOS YA DEFINIDOS
+print("\n")
+print ("A continuacion se muestra como quedo la tabla luego de calcular los porcentajes\n")
+print("La tabla queda de la siguiente manera")
+print ("|   EQUIPO    |   CAMPEONATOS GANADOS\t|   PORCENTAJE   |")
+print ("|  America    |            10 \t\t|       21.74%\t |")
+print ("| Cruz Azul   |             8\t\t|       17.39%\t |")
+print ("| Guadalajra  |            11 \t\t|       23.91%\t |")
+print ("|  Toluca     |            10 \t\t|       21.74%\t |")
+print ("|   UNAM      |             7\t\t|       15.22%\t |")
+print ("|  TOTAL      |            46 \t\t|        100%\t |")
+
+#Espero les agrade, cualquier cosa diganme que onda y si pueden ayudarme a mejorarlo hagan paro porfa 
