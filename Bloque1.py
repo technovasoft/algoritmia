58917bfb7e0e8ea582b3834eaf4efd529a8f91f4
Carlos Hernández <carlos.hernandez.sc@itszapopan.edu.mx>
2015-10-20 18:20:43 -0500

    Update Bloque1.py

--- Bloque1.py
+++ Bloque1.py
@@ -1,44 +1,63 @@
-print ("BLOQUE 1")
+print("BLOQUE 1")
 print ("CONCEPTOS GENERALES\n")
+print ("         EJERCICIOS DE REPRESENTACION E\nINTERPRETACION DE GRAFICAS PARA DATOS NO AGRUPADOS\n") 
+print ("Hola :D")
+#######ALGORITMO
+print ("Ingresa el nombre de los equipos y los campeonatos ganados a continuacion:")
+n1 = input ("Primer equipo: \n>>> ")
+c1 = input ("campeonatos Primer equipo: \n>>> ")
+n2 = input ("Segundo equipo: \n>>> ")
+c2 = input ("campeonatos Segundo equipo: \n>>> ")
+n3 = input ("Tercer equipo: \n>>> ")
+c3 = input ("campeonatos Tercer equipo: \n>>> ")
+n4 = input ("Cuarto equipo: \n>>> ")
+c4 = input ("campeonatos Cuarto equipo: \n>>> ")
+n5 = input ("Quinto equipo: \n>>> ")
+c5 = input ("campeonatos Quinto equipo: \n>>> ")
+n6 = input ("Sexto equipo: \n>>> ")
+c6 = input ("campeonatos Sexto equipo: \n>>> ")
 
+c_1 = int(c1)
+c_2 = int(c2)
+c_3 = int(c3)
+c_4 = int(c4)
+c_5 = int(c5)
+c_6 = int(c6)
 
-###########ALGORITMO
-print ("DISTRIBUCION DE FRECUENCIA")
-print ("Problema 1.1 Los datos de la siguiente tabla muestran a los equipos con mas campeonatos de liga ganados en la Federacion Mexicana de Futbol hasta el año 2011\n")
-print ("|   EQUIPO    |   CAMPEONATOS GANADOS\t|   PORCENTAJE   |")
-print ("|  America    |            10 \t\t|       ------\t |")
-print ("| Cruz Azul   |             8\t\t|       ------\t |")
-print ("| Guadalajara |            11 \t\t|       ------\t |")
-print ("|  Toluca     |            10 \t\t|       ------\t |") ##Tenemos un total de 46 campeonatos 
-print ("|   UNAM      |             7\t\t|       ------\t |")
-print ("|  TOTAL      |            46 \t\t|        100%\t |\n")
+print ("| Equipo: \t Campeonatos ganados \t|")
+print ("|",n1,"\t\t",       c_1, "\t\t|")
+print ("|",n2,"\t\t",       c_2, "\t\t|")
+print ("|",n3,"\t\t",       c_3, "\t\t|")
+print ("|",n4,"\t\t",       c_4, "\t\t|")
+print ("|",n5,"\t\t",       c_5, "\t\t|")
+print ("|",n6,"\t\t",       c_6, "\t\t|")
 
-print ("*Calcula los porcentajes que corresponden a cada equipo conforme a la cantidad de campeonatos ganados\nPara calcular los porcentajes esta es la formula:\n")
-print ("EJEMPLO:")
-print ("| Cantidad de campeonatos          porcentaje \t|")
-print ("|         46 --------------------->   100% \t|")
-print ("|         10 --------------------->     x \t|\nSe multiplicara la cantidad de campeonatos ganados del equipo por 100 entre la cantidad TOTAL, osea la suma de todos los campeonatos ganados")
-print ("10*100/46 = 21.74\n")
+suma = (c_1 + c_2 + c_3 + c_4 + c_5 + c_6)
+print ("\nSuma total de todos los campeonatos: \n>>",suma)
 
-for rep in range (0,5):
-    
-    n1 = input("Ingresa la cantidad de campeonatos ganados:\n>>> ")
-    n2 = input("Ingresa la cantidad total de campeonatos ganados:\n>>> ")
-    n3 = float(n1)
-    n4 = float(n2)
-    operacion = (n3 * 100 / n4)
-    operacion2 = round(operacion,2 ) #Round sirve para redondear una cantidad que se imprime y ",2" sirve para solo imprimir dos decimales.
-    print (operacion2)
-    
-############TABLA CON LOS DATOS YA DEFINIDOS
-print("\n")
-print ("A continuacion se muestra como quedo la tabla luego de calcular los porcentajes\n")
-print("La tabla queda de la siguiente manera")
-print ("|   EQUIPO    |   CAMPEONATOS GANADOS\t|   PORCENTAJE   |")
-print ("|  America    |            10 \t\t|       21.74%\t |")
-print ("| Cruz Azul   |             8\t\t|       17.39%\t |")
-print ("| Guadalajra  |            11 \t\t|       23.91%\t |")
-print ("|  Toluca     |            10 \t\t|       21.74%\t |")
-print ("|   UNAM      |             7\t\t|       15.22%\t |")
-print ("|  TOTAL      |            46 \t\t|        100%\t |")
+#####Calcular porcentajes
+porce = (c_1 * 100 / suma)
+porce2 = round(porce,2 ) #Round sirve para redondear una cantidad que se imprime y ",2" sirve para solo imprimir dos decimales.
+print ("\nPorcentaje del Primer equipo:\t",porce2,"%")
 
+porce = (c_2 * 100 / suma)
+porce2 = round(porce,2 )
+print ("Porcentaje del Segundo equipo:\t",porce2,"%")
+
+porce = (c_3 * 100 / suma)
+porce2 = round(porce,2 )
+print ("Porcentaje del Tercer equipo:\t",porce2,"%")
+
+porce = (c_4 * 100 / suma)
+porce2 = round(porce,2 )
+print ("Porcentaje del Cuarto equipo:\t",porce2,"%")
+
+porce = (c_5 * 100 / suma)
+porce2 = round(porce,2 )
+print ("Porcentaje del Quinto equipo:\t",porce2,"%")
+
+porce = (c_6 * 100 / suma)
+porce2 = round(porce,2 )
+print ("Porcentaje del Sexto equipo:\t",porce2,"%")
+
+print ("total:\t\t\t\t 100%")
