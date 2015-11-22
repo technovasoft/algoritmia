1f88edffdc6c66263edd3407692418867c04f166
Ruiz Valdivia Jose Ernesto <senjuana@users.noreply.github.com>
2015-11-21 20:34:30 -0600

    Update Bloque3.py

--- Bloque3.py
+++ Bloque3.py
@@ -1,2 +1,20 @@
-#ya llego el chido del barrio 
-#ahora si alguien si terminara esta mierda (el miercoles o martes acabare esto)
+def rangoDatosNA():
+  ##rango para datos no agrupados
+d=int(input("Cuantos datos ingresaras: "))
+datos=[0]*d
+
+for x in range(0,d):
+    datos[x]=int(input("dato: "))
+
+mayor=0
+menor=999999999999
+for x in range(0,d):
+    if datos[x] > mayor:
+        mayor=datos[x]
+    if datos[x] < menor:
+        menor=datos[x]
+
+rango = mayor - menor
+print("EL RANGO")
+print("De este grupo de datos es:",rango)
+
