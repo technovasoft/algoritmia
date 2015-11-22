e6b477ded5997ecf8ef257380312185c52a042a2
Ruiz Valdivia Jose Ernesto <senjuana@users.noreply.github.com>
2015-11-21 21:21:20 -0600

    Update Bloque3.py

--- Bloque3.py
+++ Bloque3.py
@@ -21,30 +21,25 @@ print("De este grupo de datos es:",rango)
 #------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------
 #Desciacion media para datos no agrupados
-def ValorAbsoluto(X):
-    if X < 0:
-        return X * -1
-    elif X > 0:
-        return X
-    else:
-        return 0
-    
-d=int(input("cuantos datos entraran: "))
-datos=[0]*d
-for x in range(0,d):
-    datos[x]=int(input("dato:"))
-
-j=np.mean(datos)
-suma=0
-
-for x in range(0,d):
-    i=datos[x]
-    suma+=ValorAbsoluto(i-j)
-
-
-desviacion=suma/d
-
-print("La desviacion es:",desviacion)
+def DesviacionMedia():
+  import math as mt
+  import numpy as np
+  d=int(input("cuantos datos entraran: "))
+  datos=[0]*d
+  for x in range(0,d):
+      datos[x]=int(input("dato:"))
+  
+  j=np.mean(datos)
+  suma=0
+  
+  for x in range(0,d):
+      i=datos[x]
+      suma+=mt.fabs(i-j)
+  
+  
+  desviacion=suma/d
+  
+  print("La desviacion es:",desviacion)
 
 
 
