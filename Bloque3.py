b8a63977ea077f30ffa218775687e19c51f4604e
Ruiz Valdivia Jose Ernesto <senjuana@users.noreply.github.com>
2015-11-21 21:09:07 -0600

    Update Bloque3.py

--- Bloque3.py
+++ Bloque3.py
@@ -18,3 +18,35 @@ rango = mayor - menor
 print("EL RANGO")
 print("De este grupo de datos es:",rango)
 
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+#Desciacion media para datos no agrupados
+def ValorAbsoluto(X):
+    if X < 0:
+        return X * -1
+    elif X > 0:
+        return X
+    else:
+        return 0
+    
+d=int(input("cuantos datos entraran: "))
+datos=[0]*d
+for x in range(0,d):
+    datos[x]=int(input("dato:"))
+
+j=np.mean(datos)
+suma=0
+
+for x in range(0,d):
+    i=datos[x]
+    suma+=ValorAbsoluto(i-j)
+
+
+desviacion=suma/d
+
+print("La desviacion es:",desviacion)
+
+
+
+#--------------------------------------------------------------------
+#--------------------------------------------------------------------
