93cd4f916fdc0c4b40069d20302d83f318d6a2cf
Ruiz Valdivia Jose Ernesto <senjuana@users.noreply.github.com>
2015-11-22 15:21:39 -0600

    Update Bloque5.py

--- Bloque5.py
+++ Bloque5.py
@@ -49,3 +49,43 @@ def RegresionLine():
 
 #---------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------
+#sigue en beta este codigo
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+import matplotlib.pyplot as plt # importando matplotlib
+import seaborn as sns # importando seaborn
+import numpy as np # importando numpy
+from scipy import stats # importando scipy.stats
+import pandas as pd # importando pandas
+
+# Creando un DataFrame de pandas.
+df = pd.read_csv('http://vincentarelbundock.github.io/Rdatasets/csv/datasets/longley.csv', index_col=0)
+df.head() # longley dataset
+
+# utilizando la api de formula de statsmodels
+est = smf.ols(formula='Employed ~ GNP', data=df).fit()
+est.summary() # Employed se estima en base a GNP.
+
+
+
+# grafico de regresion. que tanto se ajusta el modelo a los datos.
+y = df.Employed  # Respuesta
+X = df.GNP  # Predictor
+X = sm.add_constant(X) # agrega constante
+
+X_1 = pd.DataFrame({'GNP': np.linspace(X.GNP.min(), X.GNP.max(), 100)})
+X_1 = sm.add_constant(X_1) 
+
+y_reg = est.predict(X_1) # estimacion
+
+plt.scatter(X.GNP, y, alpha=0.3)  # grafica los puntos de datos
+plt.ylim(30, 100)  # limite de eje y
+plt.xlabel("Producto bruto") # leyenda eje x
+plt.ylabel("Empleo") # leyenda eje y
+plt.title("Ajuste de regresion") # titulo del grafico
+reg = plt.plot(X_1.GNP, y_reg, 'r', alpha=0.9)  # linea de regresion
+
+plt.show()
+
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
