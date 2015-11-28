971c5f0d44db5218b4c0c01b96294719ce0429fd
Ruiz Valdivia Jose Ernesto <senjuana@users.noreply.github.com>
2015-11-28 13:35:18 -0600

    Update Bloque7.py

--- Bloque7.py
+++ Bloque7.py
@@ -1,1 +1,12 @@
+#-------------------7.1----------------------------------------------
 
+x = int(input("cuantos datos ingresaras:"))
+datos=[0]*x
+suma = 0
+for i in range(x):
+	datos[i] = int(input("Dato: "))
+	suma+=datos[i]
+
+print("Tienes ",suma," Opciones")
+
+#------------------------------------------------------------------
