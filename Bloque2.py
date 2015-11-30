3c9f3a8e022a570a7685281d2505e5eafed53a97
CesarRiveraSC <cesar.rivera.sc@itszapopan.edu.mx>
2015-11-30 13:34:41 -0600

    Update Bloque2.py

--- Bloque2.py
+++ Bloque2.py
@@ -1,47 +1,39 @@
-#Media Aritmetica
-def mediaAritmetica():
-	print("Dama una lista de datos")
-	data=[1,2,436,3,6,2,4]
-	#falta que los datos de la lista los ingrese el usuario
-	print (data)
-	dOrder=sorted(data)
-	
-	n=len(dOrder) 
-	print ('Mediana Aritmetica: ', round(sum(data)*1.0/n,2))
+#Media Aritmeritica
+print ("Media Aritmetica:")
+print("Ingresa el numero de datos que ingresaras")
+x = int(input(">"))
+datos = [0]*x
+for i in range(x):
+    datos[i]=float(input("dato >"))
+suma=0
+for i in range(x):
+    suma+=datos[i]
+ari = suma/x
+print("Suma:",suma)
+print ("La media aritmertica es:",ari)
+
+
 
 #--------------------------------------------------------------------------------------------
 
 #Mediana 
-def mediana():
+print ("Mediana")
+print ("Corresponde al número intermedio después de ordenar los datos en forma ascendente o descendente.\n")
+print ("Ingresa el numero de datos que ingresaras")
+x = int(input(">"))
+print("ingesa los datos para ordenar")
+lista = [0]*x
+for i in range(x):
+  lista[i]=int(input(">"))
+  for recorrido in range(1,len(lista)):
+    for posicion in range(len(lista) - recorrido):
+      if lista[posicion] > lista[posicion + 1]:
+          temp = lista[posicion]
+          lista[posicion] = lista[posicion + 1]
+          lista[posicion + 1] = temp
+print ("La lista ordenada es:", lista)
+print ("La mediana es:",lista[2])
 
-	print ("Mediana")
-	print ("Corresponde al número intermedio después de ordenar los datos en forma ascendente o descendente.\n")
-	print ("Ingresa 5 numeros los cuales deseas ordenar")
-	n1 = input("Ingresa el primer numero\n>")
-	n2 = input("Ingresa el segundo numero\n>")
-	n3 = input("Ingresa el tercer numero\n>")
-	n4 = input("Ingresa el cuarto numero\n>")
-	n5 = input("Ingresa el quinto numero\n>")
-
-	n1_1 = int(n1)
-	n2_2 = int(n2)
-	n3_3 = int(n3)
-	n4_4 = int(n4)
-	n5_5 = int(n5)
-
-	lista = [n1_1,n2_2,n3_3,n4_4,n5_5]
-	for recorrido in range(1,len(lista)):
-	  for posicion in range(len(lista) - recorrido):
-	      if lista[posicion] > lista[posicion + 1]:
-	          temp = lista[posicion]
-	          lista[posicion] = lista[posicion + 1]
-	          lista[posicion + 1] = temp
-	print ("La lista ordenada es:", lista)
-	print ("La mediana es:",lista[2])
-#Esto pude ser reducido a esto 
-#from statistics import median
-#median([1, 3, 5])
-#pero aun asi usaremos lo de cesar n.n
 
 #--------------------------------------------------------------------------------------------
 #Moda
@@ -58,105 +50,18 @@ def moda():
 #---------------------------------------------------------------------------------------------
 
 import math
-print ("MEDIA GEOMÉTRICA")
-print ("La media geometrica de una cantidad finita de números(digamos N números)\nes la raíz enésima del producto de todos los números.")
-
-print ("n√Σ x")
-
-numero = input("ingresa el numero de n del 1 al 6\n>")
-num = int(numero)
-
-if (num == 1):
-    print ("Haz seleccionado 1")
-    x = input("Ingresa el numero que desees calcular\n")
-    x1 = float(x)
-    raiz = (x1 ** (1/1))
-    print (raiz)
-
-elif (num == 2):
-    print ("Haz seleccionado 2")
-    x = input ("Ingresa el primer numero\n>")
-    x2 = input ("Ingresa el segundo numero\n>")
-    x_ = float (x)
-    x_2=float (x2)
-    mult = (x_ * x_2)
-    print ("=2√",x,"x",x2)
-    print ("=2√",mult)
-    raiz = (mult ** (1/2))
-    print ("El resultado es:",raiz)
-
-elif (num == 3):
-    print ("Haz seleccionado 3")
-    x  = input ("Ingresa el primer numero\n>")
-    x2 = input ("Ingresa el segundo numero\n>")
-    x3 = input ("Ingresa el tercer numero\n>")
-    x_= float (x)
-    x_2 = float (x2)
-    x_3 = float (x3)
-    mult = (x_ * x_2 * x_3)
-    print ("=3√",x,"x",x2,"x",x3)
-    print ("=3√",mult)
-    raiz = (mult ** (1/3))
-    print ("El resultado es:",raiz)
-
-elif (num == 4):
-    print ("Haz seleccionado 4")
-    x  = input ("Ingresa el primer numero\n>")
-    x2 = input ("Ingresa el segundo numero\n>")
-    x3 = input ("Ingresa el tercer numero\n>")
-    x4 = input ("Ingresa el cuarto numero\n>")
-    x_= float (x)
-    x_2 = float (x2)
-    x_3 = float (x3)
-    x_4 = float (x4)
-    mult = (x_ * x_2 * x_3 * x_4)
-    print ("=4√",x,"x",x2,"x",x3,"x",x4)
-    print ("=4√",mult)
-    raiz = (mult ** (1/4))
-    print ("El resultado es:",raiz)
-
-elif (num == 5):
-    print ("Haz seleccionado 5")
-    x  = input ("Ingresa el primer numero\n>")
-    x2 = input ("Ingresa el segundo numero\n>")
-    x3 = input ("Ingresa el tercer numero\n>")
-    x4 = input ("Ingresa el cuarto numero\n>")
-    x5 = input ("Ingresa el quinto numero\n>")
-    x_= float (x)
-    x_2 = float (x2)
-    x_3 = float (x3)
-    x_4 = float (x4)
-    x_5 = float (x5)
-    mult = (x_ * x_2 * x_3 * x_4 * x_5)
-    print ("=5√",x,"x",x2,"x",x3,"x",x4,"x",x5)
-    print ("=5√",mult)
-    raiz = (mult ** (1/5))
-    print ("El resultado es:",raiz)
-
-elif (num == 6):
-    print ("Haz seleccionado 6")
-    x  = input ("Ingresa el primer numero\n>")
-    x2 = input ("Ingresa el segundo numero\n>")
-    x3 = input ("Ingresa el tercer numero\n>")
-    x4 = input ("Ingresa el cuarto numero\n>")
-    x5 = input ("Ingresa el quinto numero\n>")
-    x6 = input ("Ingresa el sexto numero\n>")
-    x_= float (x)
-    x_2 = float (x2)
-    x_3 = float (x3)
-    x_4 = float (x4)
-    x_5 = float (x5)
-    x_6 = float (x6)
-    mult = (x_ * x_2 * x_3 * x_4 * x_5 * x_6)
-    print ("=6√",x,"x",x2,"x",x3,"x",x4,"x",x5,"x",x6)
-    print ("=6√",mult)
-    raiz = (mult ** (1/6))
-    print ("El resultado es:",raiz)
-
-
-else:
-    print ("Caracter no valido")
-	    
+print ("Media Geometrica:")
+print("Ingresa el numero de datos que ingresaras")
+x = int(input(">"))
+datos = [0]*x
+for i in range(x):
+    datos[i]=float(input("dato >"))
+multi = 1
+for i in range(x):
+    multi*=datos[i]
+geo = math.pow(multi,1/x)
+print("√",multi)
+print ("La media aritmertica es:",geo)
 	   
 #------------------------------------------------------------------------------------------------------------------------------
 
