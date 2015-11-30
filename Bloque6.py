8acced2eb502b5a0270336d8b3399429e16ddf0f
CesarRiveraSC <cesar.rivera.sc@itszapopan.edu.mx>
2015-11-30 02:07:32 -0600

    Update Bloque6.py

--- Bloque6.py
+++ Bloque6.py
@@ -1,1 +1,30 @@
+import math
 
+print ("P= ((n!/r!(n-r)!)(p)^r*(q)^n-r)")
+
+print("n = Población o número de ensayos")
+print("r = número de exito buscado")
+print("p = probabilidad de exito")
+print("q = probabilidad de fracaso")
+
+print ("Ingresa el valor de n")
+n1 = float(input())
+n = (math.factorial(n1))
+
+print ("Ingresa el valor de r")
+r1 = float(input())
+r = (math.factorial(r1))
+
+print ("Ingresa el valor de p")
+p = float(input())
+
+print ("Ingresa el valor de q")
+q = float(input())
+
+rn = int(n1-r1)
+print(rn)
+
+nr = (math.factorial(rn))
+
+bi = ((n/(r*nr))*(p**r)*(q**rn))
+print (bi*100,"%")
